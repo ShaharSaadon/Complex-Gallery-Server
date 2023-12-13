7f6cd10bf1b96d583db61aaf63d31ebe53c5abd1
Shahar Saadon <shahars@moveo.co.il>
2023-12-13 16:23:27 +0200

    server updating

--- server.js
+++ server.js
@@ -11,37 +11,36 @@ const io = require("socket.io")(http, {
     },
 });
 require("./services/socket.service")(io);
-let corsOptions;
 
+let corsOptions = {
+    origin:
+        process.env.NODE_ENV === "production"
+            ? "*"
+            : [
+                  "http://127.0.0.1:3030",
+                  "http://localhost:3030",
+                  "http://localhost:3000",
+                  "http://localhost:4000",
+                  "http://localhost:5173",
+                  "http://127.0.0.1:5173",
+              ],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json()); // handle with put requests
+
+// Serve static files in production
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.resolve(__dirname, "public")));
-    corsOptions = {
-        origin: "*",
-    };
-} else {
-    corsOptions = {
-        origin: [
-            "http://127.0.0.1:3030",
-            "http://localhost:3030",
-            "http://localhost:3000",
-            "http://localhost:4000",
-            "http://localhost:5173",
-            "http://127.0.0.1:5173",
-        ],
-        credentials: true,
-    };
 }
-app.use(cors(corsOptions));
 
 const paintingRoutes = require("./api/painting/painting.routes");
 const departureRoutes = require("./api/departure/departure.routes");
 
-app.use(express.json()); // handle with put requests
 app.use("/api/painting", paintingRoutes);
 app.use("/api/departure", departureRoutes);
-app.get("/", (req, res) => {
-    res.send("Welcome to my server!");
-});
+
 const port = process.env.PORT || 3030;
 http.listen(port, () => {
     console.log("Server is running on port: ", port);
